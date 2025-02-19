2b49d086b832493d1e4afa84a276d7ccb30000b6
miniRoflan <skamer1233@yandex.ru>
2025-02-19 22:40:00 +0500

    add: реализовать интерфейс HistoryManager в классе InMemoryHistoryManager

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -1,4 +1,25 @@
 package managers;
 
-public class InMemoryHistoryManager {
+import tasks.Task;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private List<Task> history = new LinkedList<>();
+
+
+    @Override
+    public void addTask(Task task) {
+        if (history.size() == 10) {
+            history.removeFirst();
+        }
+
+        history.addLast(task);
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return history;
+    }
 }
