e048c2e6f4188c335ae3d7c7e0665ae924da2457
miniRoflan <skamer1233@yandex.ru>
2025-02-26 11:23:59 +0500

    change&add: изменить и добваить методы в класс InMemoryHistoryManager

--- src/mainClass/Main.java
+++ src/mainClass/Main.java
@@ -3,12 +3,13 @@ package mainClass;
 
 import enums.State;
 import managers.InMemoryTaskManager;
+import managers.Managers;
+import managers.TaskManager;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -2,24 +2,37 @@ package managers;
 
 import tasks.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    private static final int MAX_COUNT_TASKS_IN_HISTORY = 10;
+
     private List<Task> history = new LinkedList<>();
+    private Map<Integer, Integer> idAndIndexInHistory = new HashMap<>();
 
 
     @Override
     public void addTask(Task task) {
-        if (history.size() == 10) {
-            history.removeFirst();
+        remove(task.id);
+
+        if (history.size() == MAX_COUNT_TASKS_IN_HISTORY) {
+            remove(history.getFirst().id);
         }
 
+        idAndIndexInHistory.put(task.id, history.size());
         history.addLast(task);
     }
 
     @Override
+    public void remove(int id) {
+        if (idAndIndexInHistory.containsKey(id)) {
+            history.remove((int) idAndIndexInHistory.get(id));
+            idAndIndexInHistory.remove(id);
+        }
+    }
+
+    @Override
     public List<Task> getHistory() {
-        return history;
+        return new ArrayList<>(history);
     }
 }
